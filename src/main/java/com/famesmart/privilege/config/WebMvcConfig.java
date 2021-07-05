b0de870b849008a777ee8c1e37249b482001c546
jianhang.wu <1226880334@qq.com>
2021-07-05 10:44:51 +0800

    alter cors config

--- src/main/java/com/famesmart/privilege/config/WebMvcConfig.java
+++ src/main/java/com/famesmart/privilege/config/WebMvcConfig.java
@@ -34,13 +34,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
         converter.setObjectMapper(objectMapper);
         converters.add(0, converter);
     }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        long MAX_AGE_SECS = 3600;
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-                .maxAge(MAX_AGE_SECS);
-    }
 }
